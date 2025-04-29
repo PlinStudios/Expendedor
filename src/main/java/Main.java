5a1c3e2a334d344032f29ab5566f3a22cb62246e
PlinStudios <plincompany2@gmail.com>
2025-04-29 13:55:22 -0400

    try-catch en main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,31 +3,39 @@ import Productos.*;
 import myExceptions.*;
 
 public class Main {
-    public static void main(String[] args) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
-        Moneda coin = new Moneda500();
-        Expendedor exp = new Expendedor(1,300);
-        Comprador cmp = new Comprador(coin,Precios.SPRITE,exp);
-        System.out.println(cmp.queConsumiste());
-        System.out.println(cmp.cuantoVuelto());
-        cmp = new Comprador(coin,Precios.FANTA,exp);
-        System.out.println(cmp.queConsumiste());
-        System.out.println(cmp.cuantoVuelto());
-        cmp = new Comprador(coin,Precios.SPRITE,exp);
-        System.out.println(cmp.queConsumiste());
-        System.out.println(cmp.cuantoVuelto());
-        cmp = new Comprador(coin,Precios.COCA,exp);
-        System.out.println(cmp.queConsumiste());
-        System.out.println(cmp.cuantoVuelto());
-        cmp = new Comprador(coin,Precios.SNICKERS,exp);
-        System.out.println(cmp.queConsumiste());
-        System.out.println(cmp.cuantoVuelto());
-        coin=new Moneda100();
-        cmp = new Comprador(coin,Precios.SUPER8,exp);
-        System.out.println(cmp.queConsumiste());
-        System.out.println(cmp.cuantoVuelto());
-        coin=new Moneda1000();
-        cmp = new Comprador(coin,Precios.SUPER8,exp);
-        System.out.println(cmp.queConsumiste());
-        System.out.println(cmp.cuantoVuelto());
+    public static void main(String[] args){
+        try {
+            Moneda coin = new Moneda500();
+            Expendedor exp = new Expendedor(1, 300);
+            Comprador cmp = new Comprador(coin, Precios.SPRITE, exp);
+            System.out.println(cmp.queConsumiste());
+            System.out.println(cmp.cuantoVuelto());
+            cmp = new Comprador(coin, Precios.FANTA, exp);
+            System.out.println(cmp.queConsumiste());
+            System.out.println(cmp.cuantoVuelto());
+            cmp = new Comprador(coin, Precios.SPRITE, exp);
+            System.out.println(cmp.queConsumiste());
+            System.out.println(cmp.cuantoVuelto());
+            cmp = new Comprador(coin, Precios.COCA, exp);
+            System.out.println(cmp.queConsumiste());
+            System.out.println(cmp.cuantoVuelto());
+            cmp = new Comprador(coin, Precios.SNICKERS, exp);
+            System.out.println(cmp.queConsumiste());
+            System.out.println(cmp.cuantoVuelto());
+            coin = new Moneda100();
+            cmp = new Comprador(coin, Precios.SUPER8, exp);
+            System.out.println(cmp.queConsumiste());
+            System.out.println(cmp.cuantoVuelto());
+            coin = new Moneda1000();
+            cmp = new Comprador(coin, Precios.SUPER8, exp);
+            System.out.println(cmp.queConsumiste());
+            System.out.println(cmp.cuantoVuelto());
+        }catch (NoHayProductoException e){
+            System.out.println(e.getMessage());
+        }catch (PagoIncorrectoException e){
+            System.out.println(e.getMessage());
+        }catch (PagoInsuficienteException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
