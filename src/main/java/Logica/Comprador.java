9608df6bcc440f7578e8520e8ea51db4aef256ca
PlinStudios <plincompany2@gmail.com>
2025-06-08 20:23:02 -0400

    Logica: Comprador no retira las monedas automaticamente

--- src/main/java/Logica/Comprador.java
+++ src/main/java/Logica/Comprador.java
@@ -9,6 +9,7 @@ import Logica.myExceptions.*;
 public class Comprador{
     private String sonido;
     private int vuelto=0;
+    Expendedor exp;
 
     /**Hace una compra a un Logica.Expendedor, pagando con una Moneda para conseguir un Producto y retira el vuelto
      *
@@ -20,18 +21,20 @@ public class Comprador{
      * @throws PagoIncorrectoException
      */
     public Comprador(Moneda m, Precios cualProducto, Expendedor exp) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+        this.exp=exp;
         try {
-            Producto mySnack = exp.comprarProducto(m, cualProducto);
+            exp.comprarProducto(m, cualProducto);
+            Producto mySnack=exp.getProducto();
             if (mySnack != null)
                 sonido = mySnack.consumir();
-
+            /*
             while (true) {
                 Moneda monedas_vuelto = exp.getVuelto();
                 if (monedas_vuelto != null)
                     vuelto += monedas_vuelto.getValor();
                 else
                     break;
-            }
+            }*/
         }catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e){
             while (true) {
                 Moneda monedas_vuelto = exp.getVuelto();
@@ -43,6 +46,17 @@ public class Comprador{
             throw e;
         }
 
+    }
+
+    /**
+     * Saca vuelto de expendedor
+     * @return moneda de vuelto
+     */
+    public Moneda sacarVuelto(){
+        Moneda monedas_vuelto = exp.getVuelto();
+        if (monedas_vuelto != null)
+            vuelto += monedas_vuelto.getValor();
+        return monedas_vuelto;
     }
 
     /**Cuanto vuelto obtuvo despues de la compra
